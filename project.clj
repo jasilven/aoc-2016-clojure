f9f9c2a603ceb539e689525117c87c47654c6a0d
jasilven <jasilven@gmail.com>
2019-04-15 19:08:06 +0300

    use deps.edn

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject aoc-2016 "1.0.0"
-  :plugins [[lein-tools-deps "0.4.3"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :project]})
