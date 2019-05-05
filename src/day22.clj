f58a17fcacd5a3cc63acf332ef2bd7834a33ce1d
jasilven <jasilven@gmail.com>
2019-05-05 21:20:50 +0300

    part 2

--- src/day22.clj
+++ src/day22.clj
@@ -1,6 +1,5 @@
 (ns day22
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
             [clojure.math.combinatorics :as combo]))
 
 (defn parse-ints [s] (mapv #(Integer/parseInt %) (re-seq #"\d+" s)))
@@ -22,17 +21,36 @@
     (loop [[n & ns] (combo/combinations (vals nodes) 2)
            cnt 0]
       (if (nil? n) cnt
-          (let [a (first n)
-                b (second n)]
+          (let [[a b] n]
             (recur ns
                    (if (or (viable? a b) (viable? b a))
                      (inc cnt) cnt)))))))
 
+(defn solve2 [fname]
+  (let [nodes (parse-input fname)
+        large-limit (:size (apply min-key :used (vals nodes)))
+        max-x (:x (apply max-key :x (vals nodes)))]
+    (doseq [y (range (inc (:y (apply max-key :y (vals nodes)))))]
+      (doseq [x (range (inc max-x))]
+        (let [used (:used (get nodes [x y]))]
+          (cond
+            (and (= x max-x) (zero? y)) (print "G")
+            (and (zero? x) (zero? y)) (print "H")
+            (zero? used) (print "_")
+            (<= used large-limit) (print ".")
+            :else (print "#"))))
+      (println)))
+  (println "Play sliding puzzle and count the steps:")
+  (println "(+ 17 22 34 (* 34 5) 1) =" (+ 17 22 34 (* 34 5) 1))
+  (+ 17 22 34 (* 34 5) 1))
+
 (comment
   (solve1 "resources/day22-input.txt")
   ;; correct answer 864
-
+  (solve2 "resources/day22-input.txt")
+  ;; correct answer 244
   )
 
 (defn -main []
-  (println "Part 1:" (solve1 "resources/day22-input.txt")))
+  (println "Part 1:" (solve1 "resources/day22-input.txt"))
+  (println "Part 2:" (solve2 "resources/day22-input.txt")))
