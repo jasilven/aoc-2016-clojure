2e197ff3e3581a4fcd4188ee4135708d23fb84d0
jasilven <jasilven@gmail.com>
2019-06-10 09:29:27 +0300

    leiningen

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aoc-2016-clojure "1.0.0"
+  :description "Advent of Code 2016"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.priority-map "0.0.10"]
+                 [org.clojure/math.combinatorics "0.1.5"]]
+  :main leiningen.web)
